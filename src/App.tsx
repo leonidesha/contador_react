ecd591a17af2321123c4b6f68d47ec360bb0a8ba
Leonides Hidalgo <leonideha@gmail.com>
2025-06-24 11:28:23 -0400

    actualizando

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import React from "react";
+
 function App() {
-  return "Hola mundo";
+  const [valor, setValor] = React.useState(0);
+  const handleClickIncrease = () => {
+    console.log("Button clicked +");
+    setValor(valor + 1);
+  };
+  const handleClickDecrease = () => {
+    console.log("Button clicked -");
+    setValor(valor - 1);
+  };
+  const handleClickReset = () => {
+    console.log("Button clicked Reset");
+    setValor(0);
+  };
+  return (
+    <div id="app">
+      <label id="etiqueta">Número:</label>
+      <label id="etiqueta">{valor}</label>
+      <button onClick={handleClickIncrease}>+</button>
+      <button onClick={handleClickDecrease}>-</button>
+      <button onClick={handleClickReset}>R</button>
+    </div>
+  );
 }
 
 export default App;
